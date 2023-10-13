fcb8231acddb0d9e97036951156ba2229d9c20ee
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2023-10-13 10:34:40 -0300

    ASCIIRenderer tests

---
+++ examples/TheChest.ConsoleApp/UI/ContainerUI.cs
@@ -0,0 +1,19 @@
+﻿using TheChest.ConsoleApp.Containers;
+
+namespace TheChest.ConsoleApp.UI
+{
+    public class ContainerUI
+    {
+        private readonly Container container;
+
+        public ContainerUI(Container container)
+        {
+            this.container = container;
+        }
+
+        public void Render()
+        {
+
+        }
+    }
+}
