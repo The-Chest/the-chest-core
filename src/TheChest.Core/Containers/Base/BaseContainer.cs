393a383c05f4b17777fb1191160a74ad8e31b3d9
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-07-19 22:43:26 -0300

    Fix BaseContainer

--- src/TheChest.Core/Containers/Base/BaseContainer.cs
+++ src/TheChest.Core/Containers/Base/BaseContainer.cs
@@ -9,8 +9,6 @@ namespace TheChest.Core.Containers.Base
     /// <typeparam name="T">An item type</typeparam>
     public abstract class BaseContainer<T> : IContainer<T>
     {
-        protected const int DEFAULT_SLOT_COUNT = 20;
-
         public virtual ISlot<T>[] Slots
         {
             get;
@@ -19,11 +17,11 @@ namespace TheChest.Core.Containers.Base
 
         public virtual ISlot<T> this[int index] => Slots[index];
 
-        public virtual int Size => Slots?.Length ?? 0;
+        public virtual int Size => Slots.Length;
 
-        public virtual bool IsFull => Slots?.All(x => x.IsFull) ?? false;
+        public virtual bool IsFull => Slots.All(x => x.IsFull);
 
-        public virtual bool IsEmpty => Slots?.All(x => x.IsEmpty) ?? true;
+        public virtual bool IsEmpty => Slots.All(x => x.IsEmpty);
 
         /// <summary>
         /// Creates a Container with slots
@@ -31,21 +29,7 @@ namespace TheChest.Core.Containers.Base
         /// <param name="slots">An array of slots</param>
         protected BaseContainer(ISlot<T>[] slots)
         {
-            Slots = slots;
-        }
-
-        /// <summary>
-        /// Creates a Container with a number of slots
-        /// </summary>
-        /// <param name="size">Sets the amount of slots (20 if not set)</param>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
-        protected BaseContainer(int size = DEFAULT_SLOT_COUNT)
-        {
-            if (size <= 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(size), $"Invalid Container Size : {size}");
-            }
-            Slots = new ISlot<T>[size];
+            Slots = slots ?? throw new ArgumentNullException(nameof(slots));
         }
     }
 }
