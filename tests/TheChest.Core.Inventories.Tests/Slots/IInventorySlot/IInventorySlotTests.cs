8ef889b0c7078ef932a5568f113d0263ec2ba3ea
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-08-07 10:00:05 -0300

    Adds IInentorySlotTests.get_one

---
+++ tests/TheChest.Core.Inventories.Tests/Slots/IInventorySlot/IInventorySlotTests.cs
@@ -0,0 +1,12 @@
+﻿using TheChest.Core.Tests.Slots.Factories.Interfaces;
+using TheChest.Tests.Slots;
+
+namespace TheChest.Core.Inventories.Tests.Slots
+{
+    public partial class IInventorySlotTests<T> : ISlotTests<T>
+    {
+        public IInventorySlotTests(ISlotFactory<T> slotFactory, ISlotItemFactory<T> itemFactory) : base(slotFactory, itemFactory)
+        {
+        }
+    }
+}
---
+++ tests/TheChest.Core.Inventories.Tests/Slots/IInventorySlot/IInventorySlotTests.get_one.cs
@@ -0,0 +1,34 @@
+﻿using TheChest.Core.Inventories.Slots;
+
+namespace TheChest.Core.Inventories.Tests.Slots
+{
+    public partial class IInventorySlotTests<T>
+    {
+        [Test]
+        public void GetOne_NoItem_ReturnsNull()
+        {
+            var slot = new InventorySlot<T>();
+            var result = slot.GetOne();
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void GetOne_FullSlot_ReturnsItem()
+        {
+            var item = this.itemFactory.CreateItem();
+            var slot = new InventorySlot<T>(item);
+            var result = slot.GetOne();
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.EqualTo(item));
+        }
+
+        [Test]
+        public void GetOne_FullSlot_RemovesItemFromSlot()
+        {
+            var item = this.itemFactory.CreateItem();
+            var slot = new InventorySlot<T>(item);
+            slot.GetOne();
+            Assert.That(slot.Content, Is.Null);
+        }
+    }
+}
