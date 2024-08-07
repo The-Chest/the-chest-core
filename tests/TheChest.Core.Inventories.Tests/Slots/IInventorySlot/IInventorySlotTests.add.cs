e493c2140e2ae8353eb7f39b28a2e688ed96f99c
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-08-07 10:00:24 -0300

    Adds IInventorySlotTests.add

---
+++ tests/TheChest.Core.Inventories.Tests/Slots/IInventorySlot/IInventorySlotTests.add.cs
@@ -0,0 +1,41 @@
+﻿using TheChest.Core.Inventories.Slots;
+
+namespace TheChest.Core.Inventories.Tests.Slots
+{
+    public partial class IInventorySlotTests<T>
+    {
+        [Test]
+        public void Add_EmptySlot_ReturnsTrue()
+        {
+            var item = this.itemFactory.CreateItem();
+            var slot = new InventorySlot<T>();
+            var result = slot.Add(item);
+            Assert.That(result, Is.True);
+        }
+        [Test]
+        public void Add_EmptySlot_ChangesItem()
+        {
+            var item = this.itemFactory.CreateItem();
+            var slot = new InventorySlot<T>();
+            slot.Add(item);
+            Assert.That(slot.Content, Is.EqualTo(item));
+        }
+        [Test]
+        public void Add_FullSlot_ReturnsFalse()
+        {
+            var item = this.itemFactory.CreateItem();
+            var slot = new InventorySlot<T>(item);
+            var result = slot.Add(item);
+            Assert.That(result, Is.False);
+        }
+        [Test]
+        public void Add_FullSlot_DoesNotChangeItem()
+        {
+            var item = this.itemFactory.CreateItem();
+            var slot = new InventorySlot<T>(item);
+            var newItem = this.itemFactory.CreateItem();
+            slot.Add(newItem);
+            Assert.That(slot.Content, Is.Not.EqualTo(newItem));
+        }
+    }
+}
