d96721f2db8f241bfe6c9d3fde6840efe485e71b
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-08-17 13:34:12 -0300

    Adds IInventory Unit Tests

---
+++ tests/TheChest.Core.Inventories.Tests/Containers/Inventory/InventoryTests.cs
@@ -0,0 +1,13 @@
+﻿using TheChest.Core.Tests.Containers;
+
+namespace TheChest.Core.Inventories.Tests.Containers
+{
+    public abstract partial class InventoryTests<T> : IContainerTests<T>
+    {
+        protected new readonly IInventoryFactory<T> containerFactory;
+        public InventoryTests(IInventoryFactory<T> containerFactory, ISlotItemFactory<T> itemFactory) : base(containerFactory, itemFactory) 
+        { 
+            this.containerFactory = containerFactory;
+        }
+    }
+}
