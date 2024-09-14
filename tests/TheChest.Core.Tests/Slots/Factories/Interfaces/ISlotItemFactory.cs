ecd558e1ed2144af106a6a9a39ede0034d89bcd3
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-09-14 18:12:10 -0300

    Implements CreateRandom and CreateManyRandom to SlotItemFactory

--- tests/TheChest.Core.Tests/Slots/Factories/Interfaces/ISlotItemFactory.cs
+++ tests/TheChest.Core.Tests/Slots/Factories/Interfaces/ISlotItemFactory.cs
@@ -1,8 +1,8 @@
 ﻿namespace TheChest.Core.Tests.Slots.Factories.Interfaces
 {
     /// <summary>
-    /// Generic <see cref="ISlot{T}"/> item creation.
-    /// There is nothing special about this interface 
+    /// Generic item creation.
+    /// There is nothing special about this interface. 
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public interface ISlotItemFactory<T>
@@ -23,5 +23,11 @@
         /// <param name="amount">Size of the returned array of items</param>
         /// <returns>Any <see cref="T"/> Array instance</returns>
         T[] CreateMany(int amount);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        T[] CreateManyRandom(int amount);
     }
 }
--- tests/TheChest.Core.Tests/Slots/Factories/SlotItemFactory.cs
+++ tests/TheChest.Core.Tests/Slots/Factories/SlotItemFactory.cs
@@ -1,4 +1,6 @@
-﻿namespace TheChest.Core.Tests.Slots.Factories
+﻿using System.Reflection;
+
+namespace TheChest.Core.Tests.Slots.Factories
 {
     public class SlotItemFactory<T> : ISlotItemFactory<T>
     {
@@ -11,12 +13,25 @@
 
         public T CreateRandom()
         {
-            throw new NotImplementedException();
+            var type = typeof(T);
+            var instance = (T)Activator.CreateInstance(type);
+
+            //TODO: extract this logic to another class for other types of values
+            var fields = instance.GetType().GetFields(BindingFlags.NonPublic |BindingFlags.Instance);
+            foreach (var field in fields) { 
+                field.SetValue(instance, Guid.NewGuid().ToString());
+            }
+            return instance;
         }
 
         public T[] CreateMany(int amount)
         {
             return Enumerable.Repeat(CreateDefault(), amount).ToArray();
+        }
+
+        public T[] CreateManyRandom(int amount)
+        {
+            return Enumerable.Repeat(CreateRandom(), amount).ToArray();
         }
     }
 }
