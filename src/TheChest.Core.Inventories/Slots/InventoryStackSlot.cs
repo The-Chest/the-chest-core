b9064dec1fa3ec48a9ba97319b61f0ba94cc8ddc
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-09-16 13:28:27 -0300

    Adds InventoryStackSlot

---
+++ src/TheChest.Core.Inventories/Slots/InventoryStackSlot.cs
@@ -0,0 +1,11 @@
+﻿using TheChest.Core.Slots;
+
+namespace TheChest.Core.Inventories.Slots
+{
+    public class InventoryStackSlot<T> : StackSlot<T>
+    {
+        public InventoryStackSlot(T[] items) : base(items) { }
+
+        public InventoryStackSlot(T[] items, int maxStackAmount) : base(items, maxStackAmount) { }
+    }
+}
