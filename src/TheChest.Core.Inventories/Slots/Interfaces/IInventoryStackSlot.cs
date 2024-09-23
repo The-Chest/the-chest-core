59b8ea2ba03d06a137721aa2ed0e626e2fd67966
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-09-22 22:57:26 -0300

    Adds IInventoryStackSlot<T> experimental methods

--- src/TheChest.Core.Inventories/Slots/Interfaces/IInventoryStackSlot.cs
+++ src/TheChest.Core.Inventories/Slots/Interfaces/IInventoryStackSlot.cs
@@ -6,28 +6,22 @@ namespace TheChest.Core.Inventories.Slots.Interfaces
     /// Interface with methods for a basic Inventory Stackable Slot 
     /// </summary>
     /// <typeparam name="T">Item the Slot Accept</typeparam>
-    public interface IInventoryStackSlot<T> : IStackSlot<T>, IInventorySlot<T>
+    public interface IInventoryStackSlot<T> : IStackSlot<T>
     {
-        /// <summary>
-        /// Try adds an array of items inside the current slot
-        /// </summary>
-        /// <param name="items">array of items to be added</param>
-        /// <returns>returns the amount of items that couldn't be added</returns>
-        int Add(T[] items);
+        [Obsolete("Experimental")]
+        bool CanAdd(T item);
+        [Obsolete("Experimental")]
+        bool CanAdd(T[] items);
 
-        T[] Replace(T[] items);
+        void Add(ref T[] items);
 
-        /// <summary>
-        /// Returns an array of item from slot
-        /// </summary>
-        /// <param name="amount">The amount of items to be returned</param>
-        /// <returns>Returns an array from slot or an empty array if <see cref="ISlot{T}.IsEmpty"/></returns>
+        [Obsolete("Experimental")]
+        bool TryAdd(ref T[] items);
+
+        T[] Replace(ref T[] items);
+
         T[] GetAmount(int amount);
 
-        /// <summary>
-        /// Clear the slot and return all this items
-        /// </summary>
-        /// <returns>Returns all item from slot</returns>
         T[] GetAll();
     }
 }
