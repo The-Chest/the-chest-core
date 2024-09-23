22f0a5bb26c255e0ca217bba36407ccdabea4794
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-09-23 09:04:00 -0300

    Adds IInventoryStackSlotTests

---
+++ tests/TheChest.Core.Inventories.Tests/Slots/Factories/Interfaces/IInventoryStackSlotFactory.cs
@@ -0,0 +1,11 @@
+﻿using TheChest.Core.Inventories.Slots.Interfaces;
+
+namespace TheChest.Core.Inventories.Tests.Slots.Factories.Interfaces
+{
+    public interface IInventoryStackSlotFactory<T> : IStackSlotFactory<T>
+    {
+        new IInventoryStackSlot<T> EmptySlot();
+        new IInventoryStackSlot<T> WithItem(T item, int amount = 1, int maxAmount = 10);
+        new IInventoryStackSlot<T> FullSlot(T item);
+    }
+}
---
+++ tests/TheChest.Core.Inventories.Tests/Slots/IInventoryStackSlot/IInventoryStackSlotTests.cs
@@ -0,0 +1,14 @@
+﻿using TheChest.Core.Tests.Slots;
+
+namespace TheChest.Core.Inventories.Tests.Slots
+{
+    public abstract partial class IInventoryStackSlotTests<T> : IStackSlotTests<T>
+    {
+        protected new readonly IInventoryStackSlotFactory<T> slotFactory;
+
+        public IInventoryStackSlotTests(IInventoryStackSlotFactory<T> slotFactory, ISlotItemFactory<T> itemFactory) : base(slotFactory, itemFactory)
+        {
+            this.slotFactory = slotFactory;
+        }
+    }
+}
