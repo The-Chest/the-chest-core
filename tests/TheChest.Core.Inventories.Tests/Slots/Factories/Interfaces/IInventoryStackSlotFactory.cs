70617d9c6bd55c0e9a77f14a50a0c7f9b09e09ed
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-09-25 09:21:58 -0300

    Adds ItemArray params to IInventoryStackSlotFactory<T>

--- tests/TheChest.Core.Inventories.Tests/Slots/Factories/Interfaces/IInventoryStackSlotFactory.cs
+++ tests/TheChest.Core.Inventories.Tests/Slots/Factories/Interfaces/IInventoryStackSlotFactory.cs
@@ -6,6 +6,8 @@ namespace TheChest.Core.Inventories.Tests.Slots.Factories.Interfaces
     {
         new IInventoryStackSlot<T> EmptySlot();
         new IInventoryStackSlot<T> WithItem(T item, int amount = 1, int maxAmount = 10);
+        IInventoryStackSlot<T> WithItems(T[] items, int maxAmount = 10);
         new IInventoryStackSlot<T> FullSlot(T item);
+        IInventoryStackSlot<T> FullSlot(T[] items);
     }
 }
