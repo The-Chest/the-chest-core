0566af72beccd020e6852f15f1f0c30e94a134e7
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-09-29 11:53:50 -0300

    Adds IInventoryStackSlotTests<T>.GetAmount unit tests

---
+++ tests/TheChest.Core.Inventories.Tests/Slots/IInventoryStackSlot/IInventoryStackSlotTests.get_amount.cs
@@ -0,0 +1,60 @@
+﻿namespace TheChest.Core.Inventories.Tests.Slots
+{
+    public partial class IInventoryStackSlotTests<T>
+    {
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void GetAmount_InvalidAmount_ThrowsArgumentExceptions(int amount)
+        {
+            var items = this.itemFactory.CreateMany(20);
+            var slot = this.slotFactory.FullSlot(items);
+
+            var result = slot.GetAmount(amount);
+
+            Assert.That(result, Is.EquivalentTo(items));
+        }
+
+        [Test]
+        public void GetAmount_SlotWithEnoughItems_ReturnsWithAmount()
+        {
+            var items = this.itemFactory.CreateMany(20);
+            var slot = this.slotFactory.FullSlot(items);
+
+            var result = slot.GetAmount(10);
+
+            Assert.That(result, Is.EquivalentTo(items[0..9]));
+        }
+
+        [Test]
+        public void GetAmount_SlotWithNotEnoughItems_ReturnsItemsFromSlot()
+        {
+            var items = this.itemFactory.CreateMany(5);
+            var slot = this.slotFactory.WithItems(items, 20);
+
+            var result = slot.GetAmount(10);
+
+            Assert.That(result, Is.EquivalentTo(items));
+        }
+
+        [Test]
+        public void GetAmount_AmoutBiggerThanSlotMaxAmount_ReturnsAllItemsFromSlot()
+        {
+            var items = this.itemFactory.CreateMany(20);
+            var slot = this.slotFactory.WithItems(items, 20);
+
+            var result = slot.GetAmount(30);
+
+            Assert.That(result, Is.EquivalentTo(items));
+        }
+
+        [Test]
+        public void GetAmount_EmptySlot_ReturnsEmptyArray()
+        {
+            var slot = this.slotFactory.EmptySlot(20);
+
+            var result = slot.GetAmount(10);
+
+            Assert.That(result, Is.EquivalentTo(Array.Empty<T>()));
+        }
+    }
+}
