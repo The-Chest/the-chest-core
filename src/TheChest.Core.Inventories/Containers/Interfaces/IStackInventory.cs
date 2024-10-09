60f82ca104c51989503c65efda3837819d91e223
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2024-10-08 22:19:23 -0300

    Adds StackInventory

--- src/TheChest.Core.Inventories/Containers/Interfaces/IStackInventory.cs
+++ src/TheChest.Core.Inventories/Containers/Interfaces/IStackInventory.cs
@@ -4,17 +4,73 @@ namespace TheChest.Core.Inventories.Containers.Interfaces
 {
     /// <summary>
     /// Interface with methods for interaction with the Inventory using stacks
+    /// <para>
+    /// This interface is still unstable. Some methods can be moved to a separated interface.
+    /// </para>
     /// </summary>
     /// <typeparam name="T">An item type</typeparam>
-    public interface IStackInventory<T> : IInventory<T>, IStackContainer<T>
+    public interface IStackInventory<T> : IStackContainer<T>
     {
+        #region ISlot
+        /// <summary>
+        /// Gets an <see cref="item"/> inside a slot
+        /// </summary>
+        /// <param name="index">Slot's inventory to be searched</param>
+        /// <returns>Returns the item inside <paramref name="index"/> Slot</returns>
+        T? Get(int index);
+
+        /// <summary>
+        /// Search an Item from inventory
+        /// </summary>
+        /// <param name="item">The item to be searched</param>
+        /// <returns>Returns the first item founded</returns>
+        T? Get(T item);
+
+        /// <summary>
+        /// Search an amount of items in the inventory
+        /// </summary>
+        /// <param name="item">Item to be founded</param>
+        /// <param name="amount">Amount to be returned</param>
+        /// <returns>Returns the amount of items searched (or the max it can)</returns>
+        T[] Get(T item, int amount);
+
+        /// <summary>
+        /// Get all Item of the selected type from all slots
+        /// </summary>
+        /// <param name="item">Item to be search</param>
+        /// <returns>Returns a list with all items founded in the inventory</returns>
+        T[] GetAll(T item);
+
+        /// <summary>
+        /// Returns the amount of an item inside the inventory
+        /// </summary>
+        /// <param name="item">The item to de counted</param>
+        /// <returns>Returns the current amount of the item in the Inventory</returns>
+        int GetCount(T item);
+
+        /// <summary>
+        /// Adds and array of item in a avaliable <see cref="ISlot{T}"/> 
+        /// </summary>
+        /// <param name="items">Array of items to be added to any avaliable <see cref="ISlot{T}"/> founded</param>
+        /// <returns></returns>
+        T[] Add(T[] items);
+
+        /// <summary>
+        /// Adds an item in a avaliable <see cref="ISlot{T}"/> 
+        /// </summary>
+        /// <param name="item">item to be added</param>
+        /// <returns>returns true if the item could be added</returns>
+        void Add(T item);
+        #endregion
+
+        #region IStack
         /// <summary>
         /// Returns an amount of items inside the Inventory Slot
         /// </summary>
         /// <param name="index">Index of the slot</param>
         /// <param name="amount">Amount of the item to be returned</param>
         /// <returns>Returns the amount of items inside the slot (or the max it can)</returns>
-        T[] GetItemAmount(int index, int amount);
+        T[] Get(int index, int amount);
 
         /// <summary>
         /// Returns all the items from the selected slot 
@@ -24,12 +80,37 @@ namespace TheChest.Core.Inventories.Containers.Interfaces
         T[] GetAll(int index);
 
         /// <summary>
+        /// Adds an item in a specific slot
+        /// </summary>
+        /// <param name="item">item to be added</param>
+        /// <param name="index">slot where the item will be added</param>
+        /// <param name="replace"></param>
+        /// <returns>Returns the item that couldn't be added or the replaced item</returns>
+        T[] AddAt(T item, int index, bool replace = true);
+
+        /// <summary>
         /// Adds an array of item inside the inventory
         /// </summary>
         /// <param name="items">Array of item of the same type wich will be added to inventory</param>
         /// <param name="index">Wich slot the items will be added</param>
         /// <param name="replace">Defines if the current Slot item will be replaced by the <see cref="items"/> param</param>
         /// <returns>Returns a array of items replaced or could'nt be added</returns>
-        T[] AddItemAt(T[] items, int index, bool replace = true);
+        T[] AddAt(T[] items, int index, bool replace = true);
+        #endregion
+
+        #region IInteractive
+        /// <summary>
+        /// Move a item between two slots
+        /// </summary>
+        /// <param name="origin">Selected item</param>
+        /// <param name="target">Where the item will be placed</param>
+        void Move(int origin, int target);
+
+        /// <summary>
+        /// Returns every item from inventory
+        /// </summary>
+        /// <returns>Returns an Array of <see cref="{T}"/></returns>
+        T[] Clear();
+        #endregion
     }
 }
---
+++ src/TheChest.Core.Inventories/Containers/StackInventory.cs
@@ -0,0 +1,202 @@
+﻿using TheChest.Core.Containers;
+using TheChest.Core.Inventories.Containers.Interfaces;
+using TheChest.Core.Inventories.Slots.Interfaces;
+
+namespace TheChest.Core.Inventories.Containers
+{
+    public class StackInventory<T> : StackContainer<T>, IStackInventory<T>
+    {
+        protected readonly IInventoryStackSlot<T>[] slots;
+
+        public override IInventoryStackSlot<T> this[int index] => this.slots[index];
+
+        public override IInventoryStackSlot<T>[] Slots => this.slots.ToArray();
+
+        public StackInventory(IInventoryStackSlot<T>[] slots) : base(slots) 
+        {
+            this.slots = slots;
+        }
+
+        public virtual void Add(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            for (var i = 0; i < this.Size; i++)
+            {
+                if (this.slots[i].CanAdd(item))
+                {
+                    this.slots[i].Add(ref item);
+                    break; 
+                }
+            }
+        }
+
+        public virtual T[] AddAt(T item, int index, bool replace = true)
+        {
+            if (index > this.Size || index <= 0)
+                throw new IndexOutOfRangeException();
+            
+            var slot = this.Slots[index];
+
+            if (!slot.CanAdd(item) && replace)
+                return slot.Replace(ref item);
+
+            slot.Add(ref item);
+
+            return default;
+        }
+
+        public virtual T[] Add(T[] items)
+        {
+            for (var i = 0; i < this.Size; i++)
+            {
+                var slot = this.slots[i];
+                if (slot.CanAdd(items))
+                {
+                    slot.Add(ref items);
+                    if(items.Length == 0)
+                        break;
+                }
+            }
+
+            return items;
+        }
+
+        public virtual T[] AddAt(T[] items, int index, bool replace = true)
+        {
+            if (index > this.Size || index <= 0)
+                throw new IndexOutOfRangeException();
+
+            var slot = this.slots[index];
+
+            if (slot.CanAdd(items))
+                slot.Add(ref items);
+
+            if (slot.CanReplace(items))
+                return slot.Replace(ref items);
+
+            return items;
+        }
+
+        public virtual T[] Clear()
+        {
+            var items = new List<T>();
+
+            for (int i = 0; i < this.Size; i++)
+            {
+                items.AddRange(this.slots[i].GetAll());
+            }
+
+            return items.ToArray();
+        }
+
+        public virtual T[] GetAll(int index)
+        {
+            if (index > this.Size || index <= 0)
+                throw new IndexOutOfRangeException();
+
+            return this.slots[index].GetAll();
+        }
+
+        public virtual T[] GetAll(T item)
+        {
+            var items = new List<T>();
+
+            for (int i = 0; i < this.Size; i++)
+            {
+                if (this.slots[i].Contains(item))
+                {
+                    items.AddRange(this.slots[i].GetAll());
+                }
+            }
+
+            return items.ToArray();
+        }
+
+        public virtual T? Get(int index)
+        {
+            if (index > this.Size || index <= 0)
+                throw new IndexOutOfRangeException();
+            
+            return this.slots[index].Get();
+        }
+
+        public virtual T? Get(T item)
+        {
+            for (int i = 0; i < this.Size; i++)
+            {
+                if (this.slots[i].Contains(item))
+                {
+                    return this.slots[i].Get();
+                }
+            }
+            return default;
+        }
+
+        public virtual T[] Get(T item, int amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            var items = new List<T>();
+            var remainingAmount = amount;
+            for (int i = 0; i < this.Size; i++)
+            {
+                if (this.slots[i].Contains(item))
+                {
+                    var slotItems = this.slots[i].Get(remainingAmount);
+                    items.AddRange(slotItems);
+                    remainingAmount -= slotItems.Length;
+                    if (remainingAmount <= 0)
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            return items.ToArray();
+        }
+
+        public virtual T[] Get(int index, int amount)
+        {
+            if (index > this.Size || index <= 0)
+                throw new IndexOutOfRangeException();
+
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            return this.slots[index].Get(amount);
+        }
+
+        public virtual int GetCount(T item)
+        {
+            var amount = 0;
+            for (int i = 0; i < this.Size; i++)
+            {
+                if (this.slots[i].Contains(item))
+                {
+                    amount += this.slots[i].StackAmount;
+                }
+            }
+            return amount;
+        }
+
+        public virtual void Move(int origin, int target)
+        {
+            if (origin < 0 || origin >= this.Size)
+            {
+                throw new ArgumentOutOfRangeException(nameof(origin));
+            }
+            if (target < 0 || target >= this.Size)
+            {
+                throw new ArgumentOutOfRangeException(nameof(target));
+            }
+
+            var items = this.slots[origin].GetAll();
+            var oldItems = this.slots[target].Replace(ref items);
+            this.slots[origin].Replace(ref oldItems);
+        }
+    }
+}
